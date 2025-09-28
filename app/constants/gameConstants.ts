409e5b9238456ea81465998a09687e1a1bf94784
JatinPanamaxil <jatin.ghetiya@panamaxil.com>
2025-09-28 19:35:46 +0530

    Update game constants and game utilities

--- app/constants/gameConstants.ts
+++ app/constants/gameConstants.ts
@@ -154,7 +154,7 @@ export const STORAGE_KEYS = {
 
 // Constants
 export const AUTO_TAP_UNLOCK_COST = 100;
-export const AUTO_TAP_DURATION = 7200; // 2 hours
+export const AUTO_TAP_DURATION = 10800; // 3 hours
 
 // Timer configuration
 export const TIMER_CONFIG: Record<
