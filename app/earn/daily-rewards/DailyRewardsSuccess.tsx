edd9c3a50af6b6ba623fdbd0d21c6f3d855f3740
JatinNarendra <jatin.ghetiya@panamaxil.com>
2025-10-06 21:21:58 +0530

    Fix final image imports - comprehensive build success

--- app/earn/daily-rewards/DailyRewardsSuccess.tsx
+++ app/earn/daily-rewards/DailyRewardsSuccess.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Image from "next/image";
-import DailyRewardsCongratulations from "../../../public/assets/Earn/DailyRewardsCongratulations.png";
-import Close from "@/public/assets/Close.png";
+// Remove image imports - we'll use src paths instead
 import CustomYellowButton from '@/app/ui/CustomYellowButton';
 
 interface DailyRewardsSuccessProps {
@@ -25,7 +24,7 @@ const DailyRewardsSuccess: NextPage<DailyRewardsSuccessProps> = ({ coins, onClos
             onClick={onClose}
             className="absolute top-4 right-4 z-10 text-gray-400 hover:text-white"
           >
-            <Image src={Close.src} alt="Close" width={32} height={32} />
+            <Image src="/assets/Close.png" alt="Close" width={32} height={32} />
           </button>
 
           {/* Main Content */}
@@ -33,7 +32,7 @@ const DailyRewardsSuccess: NextPage<DailyRewardsSuccessProps> = ({ coins, onClos
             {/* Congratulations Image */}
             <div className="relative">
               <Image
-                src={DailyRewardsCongratulations}
+                src="/assets/Earn/DailyRewardsCongratulations.png"
                 alt="Congratulations"
                 width={200}
                 height={100}
